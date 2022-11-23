8e2fecda7dae7561cc13853bb950889911655f0b
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-11-23 13:17:45 +0000

    Add function slideDown

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,8 @@ document.addEventListener("keyup", (e) => {
         slideRight();
     } else if (e.code == "ArrowUp") {
         slideUp();
+    } else if (e.code == "ArrowDown") {
+        slideDown();
     }
 })
 
@@ -120,12 +122,33 @@ function slideUp() {
     for (let c = 0; c < columns; c++) {
         let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
         row = slide(row);
-        board[0][c] = row[0];
-        board[1][c] = row[1];
-        board[2][c] = row[2];
-        board[3][c] = row[3];
+        // board[0][c] = row[0];
+        // board[1][c] = row[1];
+        // board[2][c] = row[2];
+        // board[3][c] = row[3];
 
         for (let r= 0; r < rows; r++) {
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+function slideDown() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row.reverse();
+        row = slide(row);
+        row.reverse();
+        // board[0][c] = row[0];
+        // board[1][c] = row[1];
+        // board[2][c] = row[2];
+        // board[3][c] = row[3];
+
+        for (let r= 0; r < rows; r++) {
+            board[r][c] = row[r];
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
             updateTile(tile, num);
